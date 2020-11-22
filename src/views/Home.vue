2c5de44f99f795f07bbb8d30e0d6ccdd4fae234d
anongtf <anongtf26@gmail.com>
2020-11-22 22:12:42 +0700

    beautify homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <TopSetting :isHome="true"></TopSetting>
     <v-row
     no-gutters
-    class="px-10 pt-10"
+    class="px-lg-10 px-2 pt-10"
     >
       <v-col
       cols="12"
@@ -19,45 +19,41 @@
     </v-row>
     <v-row
     no-gutters
-    class="px-10 pt-10"
+    class="px-lg-10 px-2 pt-10"
     >
       <v-col 
-      class="pr-4"
-      cols="4">
+      :class="mid.class"
+      cols="12"
+      lg="4"
+      v-for="mid in middleData"
+      :key="mid.title">
         <v-card
-        elevation="5"
-        rounded="xl"
-        >
-          <v-card-text>
-            <h1>Rp 5.720.000</h1>
-            <p>Total Biaya Listrik</p>
-          </v-card-text>
-        </v-card>
+            color="white"
+            elevation="5"
+            rounded="xl"
+          >
+            <div class="d-flex flex-no-wrap">
+              <v-avatar
+                class="ml-5 my-auto"
+                size="50"
+              >
+                <v-img :src="mid.icon"></v-img>
+              </v-avatar>
+              <div>
+                <v-card-title class="headline" v-text="mid.data"></v-card-title>
+                <v-card-subtitle v-text="mid.title"></v-card-subtitle>
+              </div>
+            </div>
+          </v-card>
       </v-col>
-      <v-col 
-      class="pr-4"
-      cols="4">
-        <v-card
-        elevation="5"
-        rounded="xl"
-        >
-          <v-card-text>
-            <h1>Rp 5.720.000</h1>
-            <p>Total Biaya Listrik</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
+    </v-row>
+    <v-row
+    no-gutters
+    >
       <v-col
-      cols="4">
-        <v-card
-        elevation="5"
-        rounded="xl"
-        >
-          <v-card-text>
-            <h1>Rp 5.720.000</h1>
-            <p>Total Biaya Listrik</p>
-          </v-card-text>
-        </v-card>
+      cols="12"
+      >
+
       </v-col>
     </v-row>
   </v-container>
@@ -77,11 +73,34 @@ export default {
       chartData: {
         datasets: [{
           data: [100,120,135,140,160,185],
-          label: "penggunaan daya total bulan ini"
+          label: "penggunaan daya total bulan ini",
+          borderColor:'rgba(0,0,255,1)',
+          fill: false
         }],
-        labels: ["1", "2", "3", "4", "5", "6"]
+        labels: ["1", "2", "3", "4", "5", "6"],
+        responsive: true
       },
       chartHeight: 100,
+      middleData: [
+        { 
+          title: "Total Biaya Listrik", 
+          data: "Rp 5.720.000", 
+          icon: "https://cdn.vuetifyjs.com/images/cards/foster.jpg", 
+          class: "pr-lg-4 pr-0 mb-2"
+        },
+        { 
+          title: "Total Daya", 
+          data: "15.000 VA", 
+          icon: "https://cdn.vuetifyjs.com/images/cards/foster.jpg", 
+          class: "pr-lg-4 pr-0 mb-2"
+        },
+        { 
+          title: "Ruangan terboros", 
+          data: "Ruang produksi 1", 
+          icon: "https://cdn.vuetifyjs.com/images/cards/foster.jpg", 
+          class: "mb-2"
+        },
+      ]
     }),
     methods:{
       
